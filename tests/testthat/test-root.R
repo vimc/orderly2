b28a73d0cafd65c0e6ac983dca1eab7c3f98ba97
Rich FitzJohn <rich.fitzjohn@gmail.com>
2022-06-14 10:34:27 +0100

    Add root support

---
+++ tests/testthat/test-root.R
@@ -0,0 +1,41 @@
+test_that("Configuration must be empty", {
+  tmp <- tempfile()
+  on.exit(unlink(tmp, recursive = TRUE))
+  fs::dir_create(tmp)
+  writeLines("a: 1", file.path(tmp, "orderly_config.yml"))
+  expect_error(orderly_config(tmp),
+               "No configuration options are currently supported")
+})
+
+
+test_that("Configuration must exist", {
+  tmp <- tempfile()
+  on.exit(unlink(tmp, recursive = TRUE))
+  fs::dir_create(tmp)
+  outpack::outpack_init(tmp)
+  expect_error(orderly_config(tmp),
+               "File does not exist: 'orderly_config.yml'")
+  expect_error(orderly_root(tmp, FALSE),
+               "File does not exist: 'orderly_config.yml'")
+})
+
+
+test_that("Initialisation requires empty directory", {
+  tmp <- tempfile()
+  fs::dir_create(tmp)
+  on.exit(unlink(tmp, recursive = TRUE))
+  file.create(file.path(tmp, "file"))
+  expect_error(orderly_init(tmp),
+               "'path', if it already exists, must be an empty directory")
+})
+
+
+test_that("Can initialise a new orderly root", {
+  tmp <- tempfile()
+  on.exit(unlink(tmp, recursive = TRUE))
+  root <- orderly_init(tmp)
+  expect_true(file.exists(tmp))
+  expect_s3_class(root, "orderly_root")
+  expect_s3_class(root$outpack, "outpack_root")
+  expect_null(root$config)
+})
