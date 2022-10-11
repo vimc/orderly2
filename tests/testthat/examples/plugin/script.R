9e0bcae9c22baa2147b54f0157e668f77caf4da8
Rich FitzJohn <rich.fitzjohn@gmail.com>
2022-10-11 08:52:54 +0100

    Refactor helper

---
+++ tests/testthat/examples/plugin/script.R
@@ -0,0 +1,1 @@
+saveRDS(dat, 'data.rds')
--- tests/testthat/test-plugin.R
+++ tests/testthat/test-plugin.R
@@ -1,32 +1,10 @@
 test_that("Can run simple example with plugin", {
-  path <- test_prepare_orderly_example("minimal")
-
-  writeLines(c(
-    "plugins:",
-    "  example.random: ~",
-    "example.random:",
-    "  distribution:",
-    "    normal"),
-    file.path(path, "orderly_config.yml"))
-  writeLines(c(
-    "script: script.R",
-    "artefacts:",
-    "  data:",
-    "    description: Generated data",
-    "    filenames: data.rds",
-    "example.random:",
-    "  dat: 10"),
-    file.path(path, "src/minimal/orderly.yml"))
-  writeLines(
-    "saveRDS(dat, 'data.rds')",
-    file.path(path, "src/minimal/script.R"))
-
-  unlink(file.path(path, "src/minimal/data.csv"))
+  path <- test_prepare_orderly_example("plugin")
 
   register_example_plugin()
   env <- new.env()
   set.seed(1)
-  id <- orderly_run("minimal", root = path, envir = env)
+  id <- orderly_run("plugin", root = path, envir = env)
 
   set.seed(1)
   cmp <- rnorm(10)
@@ -40,6 +18,6 @@ test_that("Can run simple example with plugin", {
   ## provide deserialisers that can apply any required simplification?
   expect_equal(meta$custom$orderly$plugins$example.random,
                list(dat = list(mean(cmp), var(cmp))))
-  expect_equal(readRDS(file.path(path, "archive", "minimal", id, "data.rds")),
+  expect_equal(readRDS(file.path(path, "archive", "plugin", id, "data.rds")),
                cmp)
 })
